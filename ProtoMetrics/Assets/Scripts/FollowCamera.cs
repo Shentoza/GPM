45fa531746b214f2195beeeb4b4a3156b7fbc97d
Shentoza <wuelk@hotmail.de>
2015-10-15 19:23:42 +0200

    first commit

---
+++ ProtoMetrics/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject viewTarget;
+    public float elevation = Mathf.PI / 4.0f;
+    public float azimuth = 0.0f;
+
+    public float distance = 15.0f;
+    public float minDistance = 5.0f;
+    public float maxDistance = 30.0f;
+
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public Vector3 xDest = new Vector3();
+
+    public float distSpringConstant = 10.0f;
+    public float mass = 1.0f;
+    public bool dragged = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!viewTarget)
+            Debug.LogError("viewTarget not set! SpringFollowCamera will not work!");
+
+        xDest.x = distance * Mathf.Sin(azimuth) * Mathf.Sin(elevation);
+        xDest.y = distance * Mathf.Cos(elevation);
+        xDest.z = distance * -Mathf.Cos(azimuth) * Mathf.Sin(elevation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SetupCamera ();
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        Vector3 localX, localY, localZ;
+        localZ = viewTarget.transform.forward;
+        localY = viewTarget.transform.up;
+        localX = viewTarget.transform.right;
+
+        Vector3 xWanted = xDest.x * localX + xDest.y * localY + xDest.z * localZ;
+        xWanted += viewTarget.transform.position;
+
+        Vector3 acceleration = new Vector3(0, 0, 0);
+        acceleration += -velocity * 2.0f * Mathf.Sqrt(distSpringConstant) / mass;
+        acceleration += -(this.transform.position - xWanted) * distSpringConstant / mass;
+
+        this.transform.position += Time.deltaTime * velocity;
+        this.transform.LookAt(viewTarget.transform.position);
+
+        velocity = velocity + Time.deltaTime * acceleration;
+    }
+}
---
+++ ProtoMetrics/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+    public float baseSpeed = 2.0f;
+    public float baseAngularSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        {
+            // Debug draw for local coordinate system
+            Vector3 origin = this.transform.position + 3.0f * this.transform.up;
+            Debug.DrawLine(origin, origin + 2.0f * this.transform.right, Color.red);
+            Debug.DrawLine(origin, origin + 2.0f * this.transform.up, Color.green);
+            Debug.DrawLine(origin, origin + 2.0f * this.transform.forward, Color.blue);
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float angular = baseAngularSpeed * h;
+        Vector3 speed = baseSpeed * v * this.transform.forward;
+
+        this.transform.position += Time.deltaTime * speed; // translation based on horizontal axis
+        this.transform.Rotate(Vector3.up, angular); // rotation based ob vertical axis
+
+    }
+
+
+}
