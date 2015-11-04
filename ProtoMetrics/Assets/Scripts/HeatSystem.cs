6c6ab9bad57e0febe401dc831976ec56e824b335
Shentoza <wuelk@hotmail.de>
2015-11-04 11:11:28 +0100

    WhatToMeasure.txt,   nur Heatupdate wenn änderung von 0.1 (quote), grundlegendes level mit mauern

--- ProtoMetrics/Assets/Scripts/HeatSystem.cs
+++ ProtoMetrics/Assets/Scripts/HeatSystem.cs
@@ -16,20 +16,26 @@ public class HeatSystem : MonoBehaviour {
     public Material colorHot;
     public Material colorCold;
 
+    float oldQuote;
+
 	void Start () {
         move = (Movement)GetComponent(typeof(Movement));
         rend = (Renderer)GetComponent(typeof(Renderer));
         warming = false;
+        oldQuote = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float quote = heat / maxHeat;
-        move.speed = move.maxSpeed * quote;
-        move.angularSpeed = move.maxAngularSpeed * quote;
-
-        rend.material.Lerp(colorCold, colorHot, quote);
-
+        if (Mathf.Abs(oldQuote - quote) > .1 || warming)
+        {
+            Debug.Log("UPDATE: " + quote);
+            move.speed = move.maxSpeed * quote;
+            move.angularSpeed = move.maxAngularSpeed * quote;
+            rend.material.Lerp(colorCold, colorHot, quote);
+            oldQuote = quote;
+        }
         if(!warming)
             heat = Mathf.Clamp(heat - decayPerSecond * Time.deltaTime, 0, maxHeat);
 	}
